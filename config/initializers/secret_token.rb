681769457ba98bbea43f44399bfb37aa19ebce46
Steve Zimmerman <sez@lmf.net>
2012-01-10 10:14:18 -0500

    first commit of radish app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadishApp::Application.config.secret_token = '8d872863af42fa1f4141b70cffd0cbbb971c3dd17de013007a590365bd73bd14c6b147dc45982c5e8be8547c4c53a7a91b026a65103ad98b6a227ddf0e10886b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RadishApp::Application.config.session_store :cookie_store, key: '_radish-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RadishApp::Application.config.session_store :active_record_store
